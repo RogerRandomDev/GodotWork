c481f60d82c66ecd81de3dd7fd8a815f30b3936d
RogerRandomDev <--add>
2021-09-21 14:46:44 -0700

    added ability to change levels

--- Game/Assets/Scripts/MashBash/LoadEvent1.gd
+++ Game/Assets/Scripts/MashBash/LoadEvent1.gd
@@ -1,24 +1,33 @@
 extends Area2D
-export var eventType:String
 
-export var eventID:int
+export (Array,Array) var eventDATA
 
 func _on_Area2D_body_entered(body):
 	if body.name =="Player":
 		trigger()
 
 func trigger():
-	if get_tree().get_nodes_in_group("BottomText")[0].canchange:
-		if eventType=="AI.TEXT":
-			get_tree().get_nodes_in_group("BottomText")[0].currentset = eventID
-			get_tree().get_nodes_in_group("BottomText")[0].currenttextset = 0
-			get_tree().get_nodes_in_group("BottomText")[0].prepText()
-			get_tree().get_nodes_in_group("BottomText")[0].loadText()
-			queue_free()
-	else:
-		$Timer.start()
-
-
+	var havedone = true
+	for event in eventDATA:
+		if get_tree().get_nodes_in_group("BottomText")[0].canchange and event[0]=="AI.TEXT":
+				get_tree().get_nodes_in_group("BottomText")[0].currentset = event[1]
+				get_tree().get_nodes_in_group("BottomText")[0].currenttextset = 0
+				get_tree().get_nodes_in_group("BottomText")[0].prepText()
+				get_tree().get_nodes_in_group("BottomText")[0].loadText()
+		elif event[0]=="AI.TEXT":
+			havedone = false
+			$Timer.start()
+		if event[0]=="REMOVE.TILE" and havedone:
+			for pos in event[1]:
+				get_tree().get_nodes_in_group("map")[0].set_cellv(pos,-1)
+		if event[0]=="CHANGE.TILE" and havedone:
+			for pos in event[1]:
+				get_tree().get_nodes_in_group("map")[0].set_cellv(Vector2(pos.x,pos.y),pos.z)
+		if event[0]=="CHANGE.LEVEL":
+# warning-ignore:return_value_discarded
+			get_tree().change_scene(event[1])
+	if havedone:
+		queue_free()
 func _on_Timer_timeout():
 	trigger()
 
