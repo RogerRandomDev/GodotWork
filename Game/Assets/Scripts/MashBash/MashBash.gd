002f4e730048ffe7171c4235c091aae381fdbb6e
RogerRandomDev <--add>
2021-09-22 14:53:46 -0700

    mirrors added

--- Game/Assets/Scripts/MashBash/MashBash.gd
+++ Game/Assets/Scripts/MashBash/MashBash.gd
@@ -14,6 +14,7 @@ extends Node2D
 export var PlayerCount = 1
 const changegame = [0,1]
 const alwaysmult = [3]
+export var mashbash:NodePath
 ##sets up player view based on player count##
 func _ready():
 	##allows you to disable the shader##
@@ -21,9 +22,12 @@ func _ready():
 		$Shader.hide()
 	##sets BIOS text##
 	var file = File.new()
-	file.open("res://Assets/Scripts/MashBash/MashBash.tres",File.READ)
-	$MashBash/Viewport/RichTextLabel.text=file.get_as_text()
-	$MashBashAnim.play("MASHBASH boot")
+	if get_node_or_null(mashbash) != null:
+		file.open("res://Assets/Scripts/MashBash/MashBash.tres",File.READ)
+		$MashBash/Viewport/RichTextLabel.text=file.get_as_text()
+		$MashBashAnim.play("MASHBASH boot")
+	else:
+		$P1/Viewport/Camera2D/Node2D/TextBox/BottomText.canchange = true
 	GlobalScene.stopmusic()
 	var Map = get_tree().get_nodes_in_group("map")[0].get_parent()
 	remove_child(Map)
--- Game/Assets/Scripts/MashBash/platform.gd
+++ Game/Assets/Scripts/MashBash/platform.gd
@@ -3,17 +3,21 @@ extends StaticBody2D
 
 export var direction:Vector2
 export var time:float
+export var moveDistance:int
 export var color:int
 var startpos
 func _ready():
 	startpos = position
-	$Sprite.region_rect=Rect2((color%4)*2,16+color-(color%4),1,1)
+	$Sprite.region_rect=Rect2(1+(color%4)*2,16+(color-(color%4))/4,1,1)
 	$Timer.wait_time = time
-	$Timer.start()
+	
 	set_constant_linear_velocity(direction)
 func _physics_process(delta):
 	position+=constant_linear_velocity*delta
-
+	if position.distance_to(startpos)>moveDistance or position.distance_to(startpos) <= 0.125:
+		set_constant_linear_velocity(-constant_linear_velocity)
+		if position.distance_to(startpos) < 0.125:
+			position = startpos
 func _on_Timer_timeout():
 	set_constant_linear_velocity(-constant_linear_velocity)
 	if position.distance_to(startpos) < 0.125:
