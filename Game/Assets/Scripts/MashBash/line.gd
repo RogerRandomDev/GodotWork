aa6f28d92231cc374a4a6fc79ee5d0953e91e7dc
RogerRandomDev <--add>
2021-09-27 13:04:18 -0700

    work on 2nd level

--- Game/Assets/Scripts/MashBash/line.gd
+++ Game/Assets/Scripts/MashBash/line.gd
@@ -6,17 +6,25 @@ var player:NodePath
 var prevpoints = []
 var done = false
 var candie = false
+var stopped = false
+var latestpoint:NodePath
 # warning-ignore:unused_argument
 func _process(delta):
 	if not done:
-		if get_node_or_null(player)!= null:
+		if get_node_or_null(player)!= null and !stopped:
 			points[points.size()-1] = get_node(player).position-position
 			$RayCast2D.position = points[points.size()-2]
 			$RayCast2D.cast_to = -points[points.size()-2]+points[points.size()-1]
 			$RayCast2D.update()
 			if $RayCast2D.is_colliding():
 				if $RayCast2D.get_collider().name=="TileMap":
-					default_color = Color.red
+					if candie:
+						default_color = Color.red
+						stopped = true
+						points[points.size()-1] = points[points.size()-2]
+						candie = false
+					else:
+						candie = true
 				else:
 					default_color = Color(0,255,255,255)
 				self.update()
