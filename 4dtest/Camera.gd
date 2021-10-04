3bf83736d395d01d9c1db7d3e1a1b51dbb9d1bd2
Roger <88466461+RogerRandomDev@users.noreply.github.com>
2021-10-04 14:48:29 -0700

    added more shapes

--- 4dtest/Camera.gd
+++ 4dtest/Camera.gd
@@ -5,7 +5,7 @@ extends Camera
 # var a = 2
 # var b = "text"
 
-
+var canchange = [0,0,0]
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -17,5 +17,17 @@ func _input(event):
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	if event is InputEventMouseMotion and Input.get_mouse_mode()==Input.MOUSE_MODE_CAPTURED:
 		$ImmediateGeometry2.material_override.set_shader_param("Rotation",
-		$ImmediateGeometry2.material_override.get_shader_param("Rotation")+Vector3(event.relative.y,event.relative.x,0)*0.005)
+		$ImmediateGeometry2.material_override.get_shader_param("Rotation")+Vector3(event.relative.y,event.relative.x,0)*0.0025)
 		pass
+func _process(delta):
+	var rot = $ImmediateGeometry2.material_override.get_shader_param("WRotation")
+	if rot.x > PI*2:rot.x = -rot.x
+	if rot.y > PI*2:rot.y = -rot.y
+	if rot.z > PI*2:rot.z = -rot.z
+	$ImmediateGeometry2.material_override.set_shader_param("WRotation",rot)
+	$ImmediateGeometry2.material_override.set_shader_param("WRotation",
+	$ImmediateGeometry2.material_override.get_shader_param("WRotation")+Vector3(PI*delta*canchange[0],PI*delta*canchange[1],PI*delta*canchange[2]))
+	
+	get_parent().get_parent().get_parent().get_parent().get_child(1).value = $ImmediateGeometry2.material_override.get_shader_param("WRotation").x
+	get_parent().get_parent().get_parent().get_parent().get_child(2).value = $ImmediateGeometry2.material_override.get_shader_param("WRotation").y
+	get_parent().get_parent().get_parent().get_parent().get_child(3).value = $ImmediateGeometry2.material_override.get_shader_param("WRotation").z
--- 4dtest/Spatial.gd
+++ 4dtest/Spatial.gd
@@ -11,21 +11,40 @@ var rot
 func _ready():
 	rot = get_node(shader).material_override.get_shader_param("WRotation")
 
-
 func _on_HSlider_value_changed(value):
+	rot = get_node(shader).material_override.get_shader_param("WRotation")
 	rot.x = value
 	get_node(shader).material_override.set_shader_param("WRotation",rot)
 
 
 func _on_HSlider2_value_changed(value):
+	rot = get_node(shader).material_override.get_shader_param("WRotation")
 	rot.y = value
 	get_node(shader).material_override.set_shader_param("WRotation",rot)
 
 
 func _on_HSlider3_value_changed(value):
+	rot = get_node(shader).material_override.get_shader_param("WRotation")
 	rot.z = value
 	get_node(shader).material_override.set_shader_param("WRotation",rot)
 
 
 func _on_HSlider4_value_changed(value):
 	get_node(shader).material_override.set_shader_param("CurrentW",value)
+
+
+func _on_Button3_toggled(button_pressed):
+	get_node(shader).get_parent().canchange[2]=int(button_pressed)
+
+
+func _on_Button2_toggled(button_pressed):
+	get_node(shader).get_parent().canchange[1]=int(button_pressed)
+
+
+func _on_Button_toggled(button_pressed):
+	get_node(shader).get_parent().canchange[0]=int(button_pressed)
+
+
+func _on_OptionButton_item_selected(index):
+	if index != 0:
+		get_node(shader).material_override.set_shader_param("chosen",index-1)
