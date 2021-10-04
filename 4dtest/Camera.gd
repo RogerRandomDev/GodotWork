99e61084915be341d022f4333008b69a052ea5d3
Roger <88466461+RogerRandomDev@users.noreply.github.com>
2021-10-04 14:03:29 -0700

    4D Test finished, can render 4d hypercube
    Added editor as well, cause why not.

---
+++ 4dtest/Camera.gd
@@ -0,0 +1,21 @@
+extends Camera
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+func _input(event):
+	if Input.is_key_pressed(KEY_ESCAPE):
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_VISIBLE:
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+		else:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if event is InputEventMouseMotion and Input.get_mouse_mode()==Input.MOUSE_MODE_CAPTURED:
+		$ImmediateGeometry2.material_override.set_shader_param("Rotation",
+		$ImmediateGeometry2.material_override.get_shader_param("Rotation")+Vector3(event.relative.y,event.relative.x,0)*0.005)
+		pass
